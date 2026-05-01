ce673c551136089c0440ff7f2dbb3629c65f2ba3
Marina Emad <marina.karmals@gmail.com>
2026-05-01 15:25:33 +0300

    Add files via upload

---
+++ Comment.java
@@ -0,0 +1,12 @@
+public class Comment {
+    private String author;
+    private String text;
+
+    public Comment(String author, String text) {
+        this.author = author;
+        this.text = text;
+    }
+
+    public String getAuthor() { return author; }
+    public String getText() { return text; }
+}
